7ee8ef6683556616f88ca6af020d65d1e0fdb392
plunkettal <104715460+plunkettal@users.noreply.github.com>
2023-11-25 13:34:30 +0100

    minor bugfixes
    - splitted utils and import functions into different files
    - fixed issue with auto determination of q value for (111) peak

--- __init__.py
+++ __init__.py
@@ -4,4 +4,9 @@
 
 __author__ = """Alexander Plunkett"""
 __email__ = 'plunkett-a@hotmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
+
+
+from .nanoscatterfit import *
+from .dataimport import import_rawdata
+from .utils import cut_diffractogram
